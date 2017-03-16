f64c38222fadc18250048f83ba2986ce05422eb4
Saïd AHEMT <said.ahemt@nolaroads.com>
2017-03-16 16:11:28 +0100

    adds mystery box and rotation

---
+++ Assets/Scripts/MysteryBoxController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MysteryBoxController : MonoBehaviour {
+
+
+	void Update () {
+
+		float angle = 35 * Time.deltaTime;
+
+		transform.Rotate (new Vector3(0, angle, 0));
+	}
+}
