c12ca7aafc9c8f7d4e2ae1118614a3de9fffccc8
Saïd AHEMT <said.ahemt@nolaroads.com>
2017-03-16 12:47:51 +0100

    applies physics and input controls to the sphere

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start() {
+
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+
+		float horizontalAxis = Input.GetAxis ("Horizontal");
+		float verticalAxis = Input.GetAxis ("Vertical");
+		Vector3 forceVector = new Vector3 (horizontalAxis, 0, verticalAxis);
+
+		rb.AddForce (forceVector, ForceMode.Force);
+	}
+}
