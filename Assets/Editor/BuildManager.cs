4af1648efa717a4b90672b5201edc01579d7e401
Saïd AHEMT <said.ahemt@nolaroads.com>
2017-04-26 17:47:50 +0200

    adds BuildManager editor class

---
+++ Assets/Editor/BuildManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+public class BuildManager
+{
+    private static BuildPlayerOptions PlayerOptionsFactory(BuildTarget target)
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+
+        string[] scenes = {"Assets/Scenes/RollBallGame.unity"};
+        string buildsDirectory = "Builds/";
+
+        options.scenes = scenes;
+        options.options = BuildOptions.None;
+        options.target = target;
+        options.locationPathName = buildsDirectory + target;
+        return options;
+    }
+
+    public static void BuildIOS()
+    {
+        BuildPipeline.BuildPlayer(PlayerOptionsFactory(BuildTarget.iOS));
+    }
+}
