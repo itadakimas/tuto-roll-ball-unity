2733d2c8db27488db2d763fe0d578863ec6bfb1b
Saïd AHEMT <said.ahemt@nolaroads.com>
2017-04-28 18:34:32 +0200

    fixes export plist path definition

--- Assets/Editor/BuildManager.cs
+++ Assets/Editor/BuildManager.cs
@@ -23,11 +23,10 @@ public class BuildManager
         PBXProject project = new PBXProject();
         string xCodeDir = cwd + "/Builds/iOS";
         string pbxprojPath = xCodeDir + "/Unity-iPhone.xcodeproj/project.pbxproj";
-        string exportPlistSourcePath = cwd + "/Export.plist";
-        string exportPlistDestinationPath = xCodeDir + "/Export.plist";
+        string exportPlistPath = cwd + "/Export.plist";
 
         project.ReadFromFile(pbxprojPath);
-        project.AddFile(exportPlistSourcePath, exportPlistDestinationPath);
+        project.AddFile(exportPlistPath, exportPlistPath);
         project.WriteToFile(pbxprojPath);
     }
 
